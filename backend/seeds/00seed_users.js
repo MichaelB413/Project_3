fc4b930f966a4c9ea42b2d3f24f792e2370270ee
mart <ama2000@outlook.com>
2024-11-25 07:35:57 -0800

    half done with faker

--- backend/seeds/00seed_users.js
+++ backend/seeds/00seed_users.js
@@ -1,12 +1,15 @@
+const { faker } = require('@faker-js/faker');
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
 exports.seed = async function(knex) {
   await knex('users').del()
-  await knex('users').insert([
-    {name: 'alex', email: 'alex@alex.com'},
-    {name: 'chief keef', email: 'keef@alex.com'},
-    {name: 'gambler', email: 'gambler@alex.com'}
-  ]);
-};
+
+  const users = Array.from({ length: 1000 }, () => ({
+    name: faker.person.fullName(),
+    email: faker.internet.email()
+  }))
+
+  await knex('users').insert(users)}
+
