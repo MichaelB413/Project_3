629cb72572e0d5bf76ee2a133c8510642ea3208e
ffuerst-dev <felix.fuerst2002@outlook.com>
2024-11-25 09:33:06 -0700

    stuck on implementing faker for the user details

--- backend/migrations/20241122200106_create_details_table.js
+++ backend/migrations/20241122200106_create_details_table.js
@@ -5,9 +5,9 @@
 exports.up = function (knex) {
     return knex.schema.createTable('details', (table) => {
       table.integer('user_id').unsigned().notNullable().primary();
-      table.decimal('checking_balance', 14, 2).defaultTo(0.0).notNullable();
-      table.decimal('savings_balance', 14, 2).defaultTo(0.0).notNullable();
-      table.decimal('debt_balance', 14, 2).defaultTo(0.0).notNullable();
+      table.integer('checking_balance').notNullable();
+      table.integer('savings_balance').notNullable();
+      table.integer('debt_balance').notNullable();
       table.foreign('user_id').references('id').inTable('users') .onDelete('CASCADE').onUpdate('CASCADE');
     });
   };
--- backend/seeds/01seed_details.js
+++ backend/seeds/01seed_details.js
@@ -1,13 +1,21 @@
-exports.seed = async function (knex) {
-  try {
+const { faker } = require('@faker-js/faker');
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+
     await knex('details').del();
-    await knex('details').insert([
-      { user_id: 1, checking_balance: 100.22, savings_balance: 21.21, debt_balance: 17.38 },
-      { user_id: 2, checking_balance: 1000000.22, savings_balance: 21.21, debt_balance: 17.38 },
-      { user_id: 3, checking_balance: 1.22, savings_balance: 21000.21, debt_balance: 1700000.38 },
-    ]);
-  } catch (error) {
-    console.error('Error seeding details table:', error);
-  }
+
+    const details = Array.from({ length: 1000 }, () => {
+      checking_balance: faker.finance.amount({min: 0, max: 1000000, dec:2, symbol: '$'});
+      savings_balance: faker.finance.amount({min: 0, max: 1000000, dec:2, symbol: '$'});
+      debt_balance: faker.finance.amount({min: 0, max: 1000000, dec:2, symbol: '$'});
+    })
+
+    await knex('details').insert(details)
+
 };
+
+
 
