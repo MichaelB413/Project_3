8c38567bb90397c4e5378df0b7ecfa716d9733b2
Michael Bonner <michaelbonner413@gmail.com>
2024-11-26 12:31:50 -0500

    home page letter change

--- vite-project_3/src/App.jsx
+++ vite-project_3/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Home from "./pages/HomePage";
+import Home from "./Pages/HomePage";
 import './App.css'
 import Button from "./components/Button";
 import RouletteWheel from './RouletteWheel';
