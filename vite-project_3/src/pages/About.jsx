557675955dbd1ee172e5cdd55f664625a0afe0bf
Michael Bonner <michaelbonner413@gmail.com>
2024-11-26 17:19:49 -0500

    names

--- vite-project_3/src/pages/About.jsx
+++ vite-project_3/src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import img from 'Project_3/vite-project_3/public/photos/Screenshot 2024-11-26 at 3.33.26 PM.png';
 
 function About() {
     return (
@@ -8,10 +9,17 @@ function About() {
                 Welcome to Gambler'$ Credit Union – your new favorite banking app. We make it easy to save money or lose it all!
             </p>
 
-            <h3>What We Offer:</h3>
+            <h2>What We Offer:</h2>
             <ul>
-                <li>Money</li>
+              <li>Money</li>
             </ul>
+            <h2>Who's Who in the Zoo:</h2>
+            <p>Always Hustlin' Alex</p>
+            {/* <img src={"Project_3/vite-project_3/public/photos/Screenshot 2024-11-26 at 3.33.26 PM.png"} /> */}
+            <p>Flex'n Money Felix</p>
+            <p>No Nonsense Nicole</p>
+            <p>Mo' Money Mike</p>
+
         </div>
     );
 }
